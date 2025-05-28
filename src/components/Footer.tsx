301f896fc2396b9a0ada833626483babc003de28
Sai Rahul Urumu <sairahulurumu@gmail.com>
2025-05-28 23:07:20 +0530

    izz okayyyy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer className="p-6 border-t border-white/30 shadow-lg backdrop-blur-md bg-white/3 mt-8">
       <div className="flex justify-center gap-6 text-2xl">
         <a
-          href="https://linkedin.com/in/your-profile"
+          href="https://www.linkedin.com/in/usrahul"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-blue-400 transition"
@@ -15,7 +15,7 @@ export default function Footer() {
           <FaLinkedin />
         </a>
         <a
-          href="https://github.com/your-username"
+          href="https://github.com/usrahul1"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-gray-300 transition"
@@ -23,7 +23,7 @@ export default function Footer() {
           <FaGithub />
         </a>
         <a
-          href="https://instagram.com/your-profile"
+          href="https://www.instagram.com/usrahul_/"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-pink-400 transition"
@@ -31,7 +31,7 @@ export default function Footer() {
           <FaInstagram />
         </a>
         <a
-          href="https://leetcode.com/your-username"
+          href="https://leetcode.com/u/usrahul186/"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-yellow-400 transition"
